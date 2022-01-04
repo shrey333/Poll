203f6095bad9f15bc0dab449c9c1226b86b96954
shrey333 <shreybhadiyadara33@yahoo.com>
2022-01-04 13:00:30 +0530

    added nivo chart

--- poll/src/ResultPoll.js
+++ poll/src/ResultPoll.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { VictoryPie } from "victory";
+import { ResponsivePie } from "@nivo/pie";
+import RandomColor from "randomcolor";
 import axios from "axios";
 import "./App.css";
 
@@ -14,10 +16,21 @@ function ResultPoll() {
       .get(`http://localhost:3000/polls/fetchPoll/${params.id}`)
       .then((res) => {
         setPoll(res.data);
+        // const newData = res.data.data.map((options) => {
+        //   let dum = options.options;
+        //   return dum.map((option) => {
+        //     return { x: option.value, y: option.votes };
+        //   });
+        // });
         const newData = res.data.data.map((options) => {
           let dum = options.options;
           return dum.map((option) => {
-            return { x: option.value, y: option.votes };
+            return {
+              id: option.value,
+              label: option.value,
+              value: option.votes,
+              color: RandomColor(),
+            };
           });
         });
         setData(newData);
@@ -53,7 +66,7 @@ function ResultPoll() {
                 return (
                   <div className=" card col-12">
                     <h4 className="card-title">{card.question}</h4>
-                    <VictoryPie
+                    {/* <VictoryPie
                       padding={{ left: 50, right: 50, top: 50, bottom: 50 }}
                       data={data[i]}
                       labels={({ datum }) => `${datum.x}: ${datum.y}`}
@@ -66,7 +79,55 @@ function ResultPoll() {
                           fontWeight: "bold",
                         },
                       }}
-                    />
+                    /> */}
+                    <div style={{ height: 500, width: 1200 }}>
+                      <ResponsivePie
+                        data={data[i]}
+                        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                        startAngle={-75}
+                        cornerRadius={16}
+                        activeOuterRadiusOffset={8}
+                        colors={{ scheme: "nivo" }}
+                        borderColor={{
+                          from: "color",
+                          modifiers: [["darker", 0.2]],
+                        }}
+                        arcLinkLabelsSkipAngle={10}
+                        arcLinkLabelsTextColor="#333333"
+                        arcLinkLabelsThickness={2}
+                        arcLinkLabelsColor={{ from: "color" }}
+                        arcLabelsSkipAngle={7}
+                        arcLabelsTextColor={{
+                          from: "color",
+                          modifiers: [["darker", 2]],
+                        }}
+                        legends={[
+                          {
+                            anchor: "bottom",
+                            direction: "row",
+                            justify: false,
+                            translateX: 0,
+                            translateY: 56,
+                            itemsSpacing: 0,
+                            itemWidth: 100,
+                            itemHeight: 18,
+                            itemTextColor: "#999",
+                            itemDirection: "left-to-right",
+                            itemOpacity: 1,
+                            symbolSize: 18,
+                            symbolShape: "circle",
+                            effects: [
+                              {
+                                on: "hover",
+                                style: {
+                                  itemTextColor: "#000",
+                                },
+                              },
+                            ],
+                          },
+                        ]}
+                      />
+                    </div>
                   </div>
                 );
               })}
